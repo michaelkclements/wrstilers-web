941535343107565ec056a51f86d21facbdb8e8b7
Michael K. Clements <michaelkc@me.com>
2019-02-09 22:13:06 +0000

    Trying to fix contact again

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -15,34 +15,35 @@ export default ({ data, transition }) => (
         padding: '50px',
       }}
     >
-      <form name='contact' method='POST' data-netlify='true' data-netlify-honeypot='bot-field'>
-        <p>
-          <label>
-            Your Name: <input type='text' name='name' />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type='email' name='email' />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Role:{' '}
-            <select name='role[]' multiple>
-              <option value='leader'>Leader</option>
-              <option value='follower'>Follower</option>
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name='message' />
-          </label>
-        </p>
-        <p>
-          <button type='submit'>Send</button>
-        </p>
+      <form
+        name='contact'
+        method='post'
+        action='/thanks'
+        data-netlify='true'
+        data-netlify-honeypot='bot-field'
+      >
+        <input type='hidden' name='bot-field' />
+
+        <div className='field half first'>
+          <label htmlFor='name'>Name</label>
+          <input type='text' name='name' id='name' required />
+        </div>
+        <div className='field half'>
+          <label htmlFor='email'>Email</label>
+          <input type='text' name='email' id='email' required />
+        </div>
+        <div className='field'>
+          <label htmlFor='message'>Message</label>
+          <textarea name='message' id='message' rows='6' required />
+        </div>
+        <ul className='actions'>
+          <li>
+            <input type='submit' value='Send Message' className='special' />
+          </li>
+          <li>
+            <input type='reset' value='Clear' />
+          </li>
+        </ul>
       </form>
     </Section>
   </Layout>
